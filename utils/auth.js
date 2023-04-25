f522a802946d7014753c253d3cf5620b13ef5170
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-04-25 11:26:42 -0700

    prettier

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,5 @@
-
 const authorized = (req, res, next) => {
-    !req.session.loggedIn ? res.redirect('/login') : next();
+  !req.session.loggedIn ? res.redirect("/login") : next();
 };
 
 module.exports = authorized;
