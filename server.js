02b004007fa067286ee5456f26adc16d147952c1
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-05-01 14:40:45 -0700

    So many different ways...

--- server.js
+++ server.js
@@ -20,7 +20,17 @@ const app = express();
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+
+const router = express.Router();
+const userRoutes = require('./controllers/userRoutes');
+const homeRoutes = require('./controllers/homeRoutes');
+const dashboardRoutes = require('./controllers/dashboardRoutes');
+
+router.use('/users', userRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+app.use(router);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Welcome back, all you hepcats out there! You're listening to the smooth, sultry sounds of ${PORT}, the PORT!`));
