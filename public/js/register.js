c038d8a4d0d09749083fd326c9ae6a8f065725a7
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-05-01 11:42:34 -0700

    login and registration separated - handlebars and js both complete

---
+++ public/js/register.js
@@ -0,0 +1,26 @@
+const registerFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-register').value.trim();
+    const password = document.querySelector('#password-register').value.trim();
+
+    if (email && password) {
+        try {
+            const response = await fetch('/api/users/register', {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (response.ok) {
+                document.location.replace('/views/layouts/dashboard.handlebars');
+            } else {
+                alert('You have failed to assimilate.  Verify your inputs and join the circle.');
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+};
+
+document.querySelector('.register-form').addEventListener('submit', registerFormHandler);
